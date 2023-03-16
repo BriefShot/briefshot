af202912ce5469a753760ddfd6aad54cacb292f8
Fabio <sperini.fab@gmail.com>
2023-03-16 13:41:08 +0100

    feat: init supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +11,11 @@ Future<void> main() async {
   } else {
     await dotenv.load(fileName: ".env.development");
   }
+
+  await Supabase.initialize(
+    url: dotenv.env["SUPABASE_URL"]!,
+    anonKey: dotenv.env["SUPABASE_ANONKEY"]!,
+  );
 
   runApp(const MaterialApp(
     home: HomeScreen(),
